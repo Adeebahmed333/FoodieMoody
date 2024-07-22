d6548df8e272433f0bb25b17a18d226a575eb83a
Adeebahmed333 <adeebahmed3337@gmail.com>
2024-07-22 16:49:09 +0530

    completed header

--- app.js
+++ app.js
@@ -1,20 +1,69 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import foodieMoodyLogo from "./images/foodie-moody-logo.png";
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 
 const Title = () => (
-  <h1 id="title" key="h1">
-    Foodie Moody
-  </h1>
+  <a className="logo" href="/">
+    <img src={foodieMoodyLogo} alt="foodie moody" />
+  </a>
 );
-const ReactComponentExample = () => {
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+          <li>
+            <a href="/cart">Cart</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
   return (
     <div>
-      <Title />
-      <h1>A Simple React Component</h1>;
+      <h4>Foodie Moody</h4>
+      <p>
+        Foodie Moody is a food delivery service that delivers food to your
+        doorstep.
+      </p>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div>
+      <h4>Foodie Moody</h4>
+      <p>
+        Foodie Moody is a food delivery service that delivers food to your
+        doorstep.
+      </p>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div>
+      <HeaderComponent />
+      <Body />
+      <Footer />
     </div>
   );
 };
 
 //passing the React element to the render
-rootElement.render(<ReactComponentExample />);
+rootElement.render(<AppLayout />);
